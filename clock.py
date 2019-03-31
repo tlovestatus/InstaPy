cc4c1268922984010acf48ac6514918b58980603
tlovestatus <48553699+tlovestatus@users.noreply.github.com>
2019-03-31 19:05:07 +0500

    Create clock.py

---
+++ clock.py
@@ -0,0 +1,38 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+""" Quickstart script for InstaPy usage """
+
+# imports
+from instapy import InstaPy
+from instapy import smart_run
+from instapy import set_workspace
+import random
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=3)
+def timed_job():
+    users_to_follow = ['ramoswasoffside', 'passporttoearth', 'fav_skies', 'super_photosunsets', 'njsunrise_sunset', 'adventures_shutter', 'myskynow', 'newjerseyisbeautiful', 'igersmood', 'amazingly_sunsets', 'hey_ihadtosnapthat', 'passion_4_living_photos', 'goventureorange', 'onlythebestcapture', 'goandcapturethelight', 'bestpicturesgallery', 'rthouse']
+    random.shuffle(users_to_follow)
+    # set workspace folder at desired location (default is at your home folder)
+    set_workspace(path=None)
+
+    # get an InstaPy session!
+    session = InstaPy(username="mufnature",
+                      password="2611fine",
+                      headless_browser=False)
+
+    with smart_run(session):
+        """ Activity flow """
+        # general settings
+        session.set_dont_include(["friend1", "friend2", "friend3"])
+        session.follow_likers(users_to_follow, photos_grab_amount=1, follow_likers_per_photo=30, randomize=False, sleep_delay=60, interact=False)
+        session.unfollow_users(amount=226, allFollowing=True, style="LIFO", unfollow_after=48 * 60 * 60, sleep_delay=10)
+        # activity
+        #session.like_by_tags(["natgeo"], amount=10)
+    print('This job is run every three minutes.')
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+def scheduled_job():
+    print('This job is run every weekday at 5pm.')
+
+sched.start()
