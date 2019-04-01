9f1855d95312c0e1e16541f017a15d8bbdbb8838
tlovestatus <48553699+tlovestatus@users.noreply.github.com>
2019-04-01 16:34:01 +0500

    Update clock.py

--- clock.py
+++ clock.py
@@ -13,8 +13,8 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', hour=os.environ['hour'],minute=os.environ['minute'])
 def scheduled_job():
-    users_to_follow = ['ramoswasoffside', 'passporttoearth', 'fav_skies', 'super_photosunsets', 'njsunrise_sunset', 'adventures_shutter', 'myskynow', 'newjerseyisbeautiful', 'igersmood', 'amazingly_sunsets', 'hey_ihadtosnapthat', 'passion_4_living_photos', 'goventureorange', 'onlythebestcapture', 'goandcapturethelight', 'bestpicturesgallery', 'rthouse']
-    random.shuffle(users_to_follow)
+    follow_likers_of_users = os.environ['follow_likers_of_users']
+    random.shuffle(follow_likers_of_users)
     # set workspace folder at desired location (default is at your home folder)
     set_workspace(path="./")
     workspace_in_use = get_workspace()
@@ -29,8 +29,8 @@ def scheduled_job():
         """ Activity flow """
         # general settings
         session.set_dont_include(["friend1", "friend2", "friend3"])
-        #session.follow_likers(users_to_follow, photos_grab_amount=1, follow_likers_per_photo=14, randomize=False, sleep_delay=60, interact=False)
-        session.unfollow_users(amount=250, allFollowing=True, style="LIFO", unfollow_after=48 * 60 * 60, sleep_delay=random.randint(1, 10)*60)
+        #session.follow_likers(follow_likers_of_users, photos_grab_amount=1, follow_likers_per_photo=14, randomize=False, sleep_delay=60, interact=False)
+        session.unfollow_users(amount=int(os.environ['unfollow_amount']), allFollowing=True, style="LIFO", unfollow_after=48 * 60 * 60, sleep_delay=random.randint(1, 10)*60)
         # activity
         #session.like_by_tags(["natgeo"], amount=10)
 
